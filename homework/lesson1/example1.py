17f1a8af353928c0e6b0d255ce245ee50ce60252
Lilit <lilit.gevorgyan@qlearsite.com>
2022-01-13 12:06:46 +0200

    lesson1 class

---
+++ homework/lesson1/example1.py
@@ -0,0 +1,56 @@
+int1 = 10
+int2 = 22
+int3 = 7
+
+count = int1 + int2
+print(count)
+
+count = int1 - int2
+print(count)
+
+count = int1 * int3
+print(count)
+
+count = int1 / int2
+print(count)
+
+count = int1 // int1
+print(count)
+
+count = int3 % int2
+print(count)
+
+count = int1 ** int3
+print(count)
+
+print(type(count))
+print(type(count) == type(1.3))
+
+bool1 = True
+bool2 = False
+
+bool1 = int1 > int2
+print(bool1)
+
+bool2 = int1 < int2
+print(bool1)
+
+bool2 = int1 = int2
+print(bool1)
+
+bool2 = int1 != int2
+print(bool1)
+
+bool2 = int1 <= int2
+print(bool1)
+
+bool2 = int1 >= int2
+print(bool1)
+
+string1 = 'nice'
+string2 = 'best'
+string3 = 'better'
+
+print(string3)
+print(string2 + string1)
+
