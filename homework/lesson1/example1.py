c58033f6a2af77d82d8432ff1bed40b3b4a76ed5
Lilit <lilit.gevorgyan@qlearsite.com>
2022-01-13 14:57:20 +0200

    homework1 done

--- homework/lesson1/example1.py
+++ homework/lesson1/example1.py
@@ -1,56 +1,117 @@
-int1 = 10
-int2 = 22
-int3 = 7
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+#
+# abc = 20
+# aBc = 20
+#
+# abc1 = 20   # 555
+# # 1abc = 20
+# # !abc = 20
+#
+# students_counter = 20
+#
+# print(students_counter)
 
-count = int1 + int2
-print(count)
+# int
 
-count = int1 - int2
-print(count)
+my_int1 = 10
+my_int2 = 6
+print(my_int1)
 
-count = int1 * int3
-print(count)
+res = my_int1 + my_int2
+print(res)
 
-count = int1 / int2
-print(count)
+res = my_int1 - my_int2
+print(res)
 
-count = int1 // int1
-print(count)
+res = my_int1 * my_int2
+print(res)
 
-count = int3 % int2
-print(count)
+res = my_int1 / my_int2
+print(res)
 
-count = int1 ** int3
-print(count)
+res = my_int1 // my_int2
+print(res)
 
-print(type(count))
-print(type(count) == type(1.3))
+res = my_int1 % my_int2
+print(res)
 
-bool1 = True
-bool2 = False
+res = my_int1 ** my_int2
+print(res)
 
-bool1 = int1 > int2
-print(bool1)
 
-bool2 = int1 < int2
-print(bool1)
+# float
 
-bool2 = int1 = int2
-print(bool1)
+my_float1 = 1.3
+my_float2 = 1.3
 
-bool2 = int1 != int2
-print(bool1)
+res = my_int1 + my_float1
+print(res)
 
-bool2 = int1 <= int2
-print(bool1)
+res = my_int1 - my_float2
+print(res)
 
-bool2 = int1 >= int2
-print(bool1)
+res = my_float2 * my_int2
+print(res)
 
-string1 = 'nice'
-string2 = 'best'
-string3 = 'better'
+res = my_int1 / my_float2
+print(res)
 
-print(string3)
-print(string2 + string1)
+res = my_float2 // my_int2
+print(res)
 
+res = my_int1 % my_float2
+print(res)
+
+res = my_float2 ** my_int2
+print(res)
+
+res = 10 / 0.5
+print(res)
+
+print(0.1 + 0.2)
+
+print((0.1 + 0.2) == 0.3)
+
+my_float2 = 2e-4
+
+print(type(my_float2))
+
+print(type(my_float2) == type(1.3))
+
+# bool
+
+my_bool1 = True  # 1
+my_bool2 = False  # 0
+
+
+my_bool2 = my_int2 > my_int1
+print(my_bool2)
+my_bool2 = my_int2 < my_int1
+print(my_bool2)
+my_bool2 = my_int2 >= my_int1
+print(my_bool2)
+my_bool2 = my_int2 <= my_int1
+print(my_bool2)
+my_bool2 = my_int2 == my_int1
+print(my_bool2)
+my_bool2 = my_int2 != my_int1
+print(my_bool2)
+
+# string
+
+my_str = 'qwerty mnbv23456 7kjhgf'
+print(my_str)
+my_str = ''
+print(my_str)
+
+my_str = 'abc'
+print(my_str + '123')
+
+print('abc' * 3)
+
+my_str = str(1.23)
+print(my_str)
+print('abc' + my_str)
---
+++ homework/lesson1/homework1.py
@@ -0,0 +1,70 @@
+# 1 Задача: Создать две переменные first=10, second=30.
+# Вывести на экран результат математического взаимодействия
+# (+, -, *, / и тд.) для этих чисел.
+
+first = 10
+second = 30
+
+calculation_value1 = first + second
+print(calculation_value1)
+
+calculation_value2 = first - second
+print(calculation_value2)
+
+calculation_value3 = first * second
+print(calculation_value3)
+
+calculation_value4 = first / second
+print(calculation_value4)
+
+calculation_value5 = first // second
+print(calculation_value5)
+
+calculation_value6 = first % second
+print(calculation_value6)
+
+calculation_value7 = first ** second
+print(calculation_value7)
+
+# 2 Задача:Создать переменную и поочередно записать в нее результат
+# сравнения (<, > , ==, !=) чисел из задания 1.
+# После этого вывести на экран значение каждой полученной переменной.
+
+
+comparison_value = first < second
+print(comparison_value)
+
+comparison_value = first > second
+print(comparison_value)
+
+comparison_value = first >= second
+print(comparison_value)
+
+comparison_value = first <= second
+print(comparison_value)
+
+comparison_value = first != second
+print(comparison_value)
+
+comparison_value = first == second
+print(comparison_value)
+
+# 4 Задача: Создать переменную - результат
+# конкатенации (сложения) строк str1="Hello " и str2="world".
+# Вывести на ее экран
+
+str1 = 'Hello '
+str2 = 'world'
+
+text = str1 + str2
+print(text)
+
+text = 'Hello ' + str2
+print(text)
+
+text = str1 + 'world'
+print(text)
+
+text = ('Hello ' + 'world')
+print(text)
+
